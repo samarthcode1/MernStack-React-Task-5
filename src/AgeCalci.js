7a9cac1b68482f263b592dd239f34d0d7bbf2a05
samarthcode1 <nagsamarth3@gmail.com>
2023-10-14 20:48:31 +0530

    first commit

---
+++ src/AgeCalci.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+function AgeCalci() {
+  const [birthdate, setBirthdate] = useState('');
+  const [age, setAge] = useState(null);
+
+  const calculateAge = () => {
+    const today = new Date();
+    const birthdateDate = new Date(birthdate);
+    const ageInMilliseconds = today - birthdateDate;
+    const ageInYears = Math.floor(ageInMilliseconds / (365 * 24 * 60 * 60 * 1000));
+    setAge(ageInYears);
+  };
+
+  return (
+    <div class="d1">
+      <h1>Age Calculator</h1>
+      <h3>Enter your Date of Birth</h3>
+      <input class="i1"
+        type="date"
+        value={birthdate}
+        onChange={(e) => setBirthdate(e.target.value)}
+      />
+
+      <button class="b1" onClick={calculateAge}>Calculate Age</button>
+      {age !== null && <p class="p1">Your age is: {age} years</p>}
+    </div>
+  );
+}
+
+export default AgeCalci;
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AgeCalci from './AgeCalci';
+
+function App() {
+  return (
+    <div className="App">
+      <center><AgeCalci /></center>
+    </div>
+  );
+}
+
+export default App;
